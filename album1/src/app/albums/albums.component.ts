744a6f7964c0305be908be46e1f6db91406df9a6
Ayaulim2023 <ayaulim1002k@gmail.com>
2024-03-28 17:06:43 +0600

    lab6

---
+++ album1/src/app/albums/albums.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Albums } from '../models';
+import { POSTS } from '../fake-db';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AlbumsService } from '../albums.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-albums',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './albums.component.html',
+  styleUrl: './albums.component.css'
+})
+
+export class AlbumsComponent implements OnInit {
+  albums!: Albums[];
+  newAlbum!: Albums;
+  loaded:boolean = false;
+
+  constructor(private albumsService: AlbumsService) { }
+
+  
+  ngOnInit() {
+    this.getAlbums();
+    this.newAlbum = {
+      userId: 1,
+      id: 101,
+      title: ''
+    };
+  }
+
+  addAlbum(){
+    this.albumsService.createAlbum(this.newAlbum).subscribe((album) => {
+      this.albums.push(album);
+      alert('New album added successfully!');
+    });
+    this.newAlbum.title = '';
+  }
+
+
+  getAlbums(){
+    this.loaded = false;
+    this.albumsService.getAlbums().subscribe((albums) => 
+    { 
+      this.albums = albums;
+      this.loaded = true
+    });
+  }
+
+  deleteAlbum(id: number){
+   
+    this.albumsService.deleteAlbum(id).subscribe((response)=>{
+      if(response) {
+        this.albums = this.albums.filter(album => album.id !== id);
+      }
+      // alert('Album was deleted!');
+    });
+  }
+}
